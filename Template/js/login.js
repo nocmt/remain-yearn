1539bebe4ec5e66a82f9d18cd05c99c8f01a5edf
nocmt <nocmt01@163.com>
2018-02-02 17:31:01 +0800

    login.html完成

---
+++ Template/js/login.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    // 判断登录按钮可否点击--第一种方式
+    // $("#input-key").on("change", function () {
+    //     let key = $(this).val();
+    //     console.log(key);
+    //     if (key) {
+    //         $("#btn-login").toggleClass("btn-off").removeAttr("disabled");
+    //     } else {
+    //         $("#btn-login").toggleClass("btn-off").attr("disabled", "true");
+    //     }
+    // });
+    // 判断登录按钮可否点击--第二种方式
+    $("#input-key").on("keyup", function () {
+        let key = $(this).val();
+        // console.log(key);
+        if (key) {
+            $("#btn-login").removeClass("btn-off").removeAttr("disabled");
+        } else {
+            $("#btn-login").addClass("btn-off").attr("disabled", "true");
+        }
+    });
+
+    // 登录事件
+    function sendkey() {
+        // 此处应该有Ajax事件
+        alert('登录成功！');
+    }
+
+    // 登录回车事件
+    $(window).keydown(function (event) {
+        if (event.keyCode === 13) {
+            let key = $(this).val();
+            if (key) {
+                sendkey();
+            } else {
+                alert('口令不能为空！请重新填写！')
+            }
+        }
+    });
+
+    $("#btn-login").on("click", function () {
+        sendkey();
+    });
+});
